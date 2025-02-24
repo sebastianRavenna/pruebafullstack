32da5aaf6ef1de5a98cea5b2f304ccf201a06d96
Sebastian Ravenna <sebastian.ravenna@gmail.com>
2025-02-24 20:34:23 -0300

    prueba v6

--- backend/src/routes/dataRoutes.js
+++ backend/src/routes/dataRoutes.js
@@ -4,6 +4,6 @@ import { getData, postData } from "../controllers/dataControllers.js";
 const dataRoutes = express.Router();
 
 dataRoutes.get("/", getData);
-dataRoutes.post("/", postData);
+dataRoutes.post("/", postData); 
 
 export { dataRoutes };
