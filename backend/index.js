9bc8f5f305c61090f0c0c56f63d3519348b34678
Sebastian Ravenna <sebastian.ravenna@gmail.com>
2025-02-25 09:16:13 -0300

    prueba db

--- backend/index.js
+++ backend/index.js
@@ -7,21 +7,31 @@ import { dataRoutes } from "./src/routes/dataRoutes.js";
 dotenv.config(); // Cargar variables de entorno
 
 const PORT = process.env.PORT || 5000;
-
 const app = express();
+
+connectDB();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Rutas
+app.use("/api/data", dataRoutes);
+
 app.get("/", (req, res) => {
   res.send("Hola mundo");
 });
 
-// Rutas
-app.use("/api/data", dataRoutes);
+app.get("/test-db", async (req, res) => {
+  try {
+    await mongoose.connection.db.admin().ping();
+    res.send("✅ Conectado a MongoDB!");
+  } catch (error) {
+    res.status(500).send("❌ No se pudo conectar a MongoDB");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
-  connectDB();
 });
+
